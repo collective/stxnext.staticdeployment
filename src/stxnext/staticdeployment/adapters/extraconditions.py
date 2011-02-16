b39209e55a8d137f8eefe65f41165fc1718821fd
Radosław Jankiewicz <radoslaw.jankiewicz@stxnext.pl>
2011-02-16 10:41:03 +0000

    added possibility to definfe custom extra condition checked before deploying each object

---
+++ src/stxnext/staticdeployment/adapters/extraconditions.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Extra deployment conditions adapters.
+"""
+from zope.interface import implements
+
+from stxnext.staticdeployment.interfaces import IExtraDeploymentCondition
+
+class ExtraDeploymentCondition(object):
+    implements(IExtraDeploymentCondition)
+
+    def __init__(self, context):
+        self.context = context
+
+    def update(self, deployment_view, modification_date):
+        self.request = deployment_view.request
+        self.deployment_view = deployment_view
+        self.config = deployment_view.config
+        self.modification_date = modification_date
+
+    def __call__(self):
+        return True
