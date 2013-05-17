083a4077dc71ac32a90aa75b0de280cfb99181ad
Mikel Larreategi <mlarreategi@codesyntax.com>
2013-05-17 08:35:29 +0200

    Revert "error control"
    This reverts commit ca4d75b0fb686cc915724fcaca60423dac8629d4.

--- src/stxnext/staticdeployment/app/util.py
+++ src/stxnext/staticdeployment/app/util.py
@@ -553,14 +553,11 @@ class StaticDeploymentUtils(object):
                             except:
                                 continue
 
-		    try:
-                        content = obj()
-                        filename = os.path.join(fullview_name, 'index.html')
-                        self._write(filename, content, None)
-                        #log.warning("Unable traverse to '%s'!" % context_path)
-                        continue
-		    except:
-		        continue
+                    content = obj()
+                    filename = os.path.join(fullview_name, 'index.html')
+                    self._write(filename, content, None)
+                    #log.warning("Unable traverse to '%s'!" % context_path)
+                    continue
 
 
             qs = {}
